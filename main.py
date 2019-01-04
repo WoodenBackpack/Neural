cad6bfdbd72157e2f5638ea9c7037ae635ff1bd7
WoodenBackpack <piotrwrobl90@gmail.com>
2019-01-04 15:31:18 +0100

    added colors

--- main.py
+++ main.py
@@ -14,50 +14,48 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         qtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-class MyScreen(qWidgets.QMainWindow):
+class MainWindow(qWidgets.QMainWindow):
     def __init__(self, parent = None):
         qWidgets.QMainWindow.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.neural = NeuralNetwork(10,3,4,2)
+    def drawNet(self):
+        circleSize = 50
+        spacing = 20
+        layerWithMaxSize = max(self.neural.layers, key=lambda x:x[0].size)
+        maxLayerLen = layerWithMaxSize.size
+        self.maxLen = maxLayerLen * (circleSize + spacing)
+        startX = circleSize
+        for layer in self.neural.layers:
+            lenSize = layer[0].size * (circleSize + spacing)
+            startPos = self.maxLen / 2 - (lenSize / 2)
+            for cell in layer[0]:
+                self.createAndDrawRect(startX, startPos, circleSize, circleSize, str(cell)[:4], self.painter, cell)
+                startPos += circleSize + spacing
+            startX += circleSize + spacing
+
     def paintEvent(self, event):
         self.painter = qtGui.QPainter(self)
-        self.painter.setPen(qtCore.Qt.black)
+        self.painter.setPen(qtCore.Qt.white)
         self.painter.begin(self)
-
-
-
-        neural = NeuralNetwork(8, 16, 2, 4)
-        maxLayerLen = max([neural.inLen[0].size, neural.hiddenLayers[0].size, neural.outLen[0].size])
-        maxLen = maxLayerLen * 40
-        lenSize = neural.inLen[0].size * 40
-        startPos = maxLen / 2 - (lenSize / 2)
-        for x in neural.inLen[0]:
-            self.createAndDrawRect(10, startPos, 30, 30, str(x)[:4], self.painter)
-            startPos += 40
-        startX = 50
-        for x in neural.hiddenLayers:
-            lenSize = x[0].size * 40
-            startPos = maxLen / 2 - (lenSize / 2)
-            for y in x[0]:
-                self.createAndDrawRect(startX, startPos, 30, 30, str(y)[:4], self.painter)
-                startPos += 40
-            startX += 40
-
-        lenSize = neural.outLen[0].size * 40
-        startPos = maxLen / 2 - (lenSize / 2)
-        for x in neural.outLen[0]:
-            self.createAndDrawRect(startX, startPos, 30, 30, str(x)[:4], self.painter)
-            startPos += 40
-
-        self.resize(500, maxLen)
+        self.drawNet();
+        self.resize(500, self.maxLen)
         self.painter.end()
-    def createAndDrawRect(self, xPos, yPos, x, y, text, painter):
+    def createAndDrawRect(self, xPos, yPos, x, y, text, painter, val):
+        color = qtGui.QColor(0, 0, 255 - int(255 * val))
         qRect = qtCore.QRect(xPos ,yPos ,x ,y)
-        self.painter.drawText(qRect, 20, text)
-        self.painter.drawRect(qRect)
+        qCircleRect = qtCore.QRect(xPos, yPos, x ,y)
+        circleSize = 50
+        center = qtCore.QPoint(xPos + 10, yPos + 25)
+        self.painter.setBrush(color)
+        self.painter.drawEllipse(qRect)
+        self.painter.setBrush(qtCore.Qt.white)
+        self.painter.drawText(center, text)
+
 
 if __name__ == "__main__":
     app = qWidgets.QApplication(["app"])
-    mainScreen = MyScreen()
+    mainScreen = MainWindow()
     mainScreen.show()
     app.exec()
--- neural.py
+++ neural.py
@@ -2,17 +2,13 @@
 import numpy as np
 
 class NeuralNetwork:
-    class Node:
-        def __init__(self):
-            self.val = np.random.uniform(0, 1)
-            print(self.val)
-
-
     def __init__(self, inputLayerLen, hiddenLayersLen, hiddenLayers, outputLayersLen):
-        self.node = self.Node()
-        self.inLen = np.random.rand(1, inputLayerLen)
-        self.hiddenLayers = []
+        self.layers = self.createAndRandomizeLayers(inputLayerLen, hiddenLayersLen, hiddenLayers, outputLayersLen)
+    def createAndRandomizeLayers(self, inputLayerLen, hiddenLayersLen, hiddenLayers, outputLayersLen):
+        inLayer = np.random.rand(1, inputLayerLen)
+        layers = [inLayer]
         for x in range(hiddenLayers):
-            self.hiddenLayers.append(np.random.rand(1, hiddenLayersLen))
-        self.outLen = np.random.rand(1, outputLayersLen)
-
+            layers.append(np.random.rand(1, hiddenLayersLen))
+        outLayer = np.random.rand(1, outputLayersLen)
+        layers.append(outLayer)
+        return layers
